68be22cabd3cccfe933d725a99b13c52448eda9a
Thijs Jonkman <thijs.jonkman@virtualsciences.nl>
2016-08-24 19:55:06 +0200

    Change raise to a numpad layer. Move home, end, pgup, pgdn to raise layer

--- keyboards/planck/keymaps/thcode/keymap.c
+++ keyboards/planck/keymaps/thcode/keymap.c
@@ -110,7 +110,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * |      | 1  ! | 2  @ | 3  # | 4  $ | 5  % | -  _ | §  ± |   {  |   }  | `  ~ |      |  § = ISO /
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      | 6  ^ | 7  & | 8  * | 9  ( | 0  ) | =  + | \  | |   [  |   ]  | \  | |Enter |  \ = ISO #
+ * |      | 6  ^ | 7  & | 8  * | 9  ( | 0  ) | =  + | \  | |   [  |   ]  | \  | |      |  \ = ISO #
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |             |      | Left | Down |  Up  |Right |
  * `-----------------------------------------------------------------------------------'
@@ -122,22 +122,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {_______, _______, _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT}
 },
 
-/* Raise
+/* Raise (Numpad)
  * ,-----------------------------------------------------------------------------------.
- * | Esc  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Del  |
+ * | Esc  |   ,  |   7  |   8  |   9  |   /  |   -  |   =  |      |      |      | Del  |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |   ~  | Mute | Vol- | Vol+ |      |      |      |   -  |   =  |   [  |   ]  |Enter |
+ * |      |   .  |   4  |   5  |   6  |   *  |   +  | Enter|      |      |      |      |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      | <<   | >||  | >>   |      |      |      |ISO # |ISO / |      |      |  \   |
+ * |      |   0  |   1  |   2  |   3  |      |      | NumL |      |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |             |      | Next | Vol- | Vol+ | Play |
+ * |      |      |      |      |      |             |      | Home | PgDn | PgUp | End  |
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = {
-  {_______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL },
-  {KC_TILD, KC_MUTE, KC_VOLD, KC_VOLU, _______, _______, _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, _______},
-  {_______, KC_MRWD, KC_MPLY, KC_MFFD, _______, _______, _______, KC_NUHS, KC_NUBS, _______, _______, KC_BSLS},
-  {_______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY}
+  {_______, KC_PCMM, KC_KP_7, KC_KP_8, KC_KP_9, KC_PSLS, KC_PMNS, KC_PEQL, _______, _______, _______, KC_DEL },
+  {_______, KC_PDOT, KC_KP_4, KC_KP_5, KC_KP_6, KC_PAST, KC_PPLS, KC_PENT, _______, _______, _______, _______},
+  {_______, KC_KP_0, KC_KP_1, KC_KP_2, KC_KP_3, _______, _______, _______, _______, _______, _______, _______},
+  {_______, _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END }
 },
 
 /* Function (Fn)
@@ -146,16 +146,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |  F1  |  F2  |  F3  |  F4  |  F5  |      |  ˂   |  ˅   |  ˃   |      |      |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Caps |  F6  |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 | Mute | Vol- | Vol+ |Enter |
+ * | Caps |  F6  |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 | Mute | Vol- | Vol+ |      |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |      |      |      |      |             |      | Home | PgDn | PgUp | End  |
+ * |      |      |      |      |      |             |      | Left | Down |  Up  |Right |
  * `-----------------------------------------------------------------------------------'
  */
 [_FUNCTION] = {
   {_______, KC_MRWD, KC_MPLY, KC_MFFD, _______, _______, _______, KC_BTN1, KC_MS_U, KC_BTN2, _______, KC_DEL },
   {_______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______, KC_MS_L, KC_MS_D, KC_MS_R, _______, _______},
   {KC_CAPS, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_MUTE, KC_VOLD, KC_VOLU, _______},
-  {_______, _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END }
+  {_______, _______, _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT}
 },
 
 /* Plover layer (http://opensteno.org)
@@ -169,7 +169,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |      |      |      |   A  |   O  |             |   E  |   U  |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
-
 [_PLOVER] = {
   {EXT_PLV, KC_1,    KC_1,    KC_1,    KC_1,    KC_1,    KC_1,    KC_1,    KC_1,    KC_1,    KC_1,    KC_1   },
   {XXXXXXX, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC},
@@ -313,36 +312,29 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   return true;
 }
 
-void matrix_init_user(void)
-{
+void matrix_init_user(void) {
     #ifdef AUDIO_ENABLE
         startup_user();
     #endif
 }
 
 #ifdef AUDIO_ENABLE
-
-void startup_user()
-{
+void startup_user() {
     _delay_ms(20); // gets rid of tick
     PLAY_NOTE_ARRAY(tone_startup, false, 0);
 }
 
-void shutdown_user()
-{
+void shutdown_user() {
     PLAY_NOTE_ARRAY(tone_goodbye, false, 0);
     _delay_ms(150);
     stop_all_notes();
 }
 
-void music_on_user(void)
-{
+void music_on_user(void) {
     music_scale_user();
 }
 
-void music_scale_user(void)
-{
+void music_scale_user(void) {
     PLAY_NOTE_ARRAY(music_scale, false, 0);
 }
-
 #endif
